8175b788d475bdd9702b5641047d261fe7f26286
emanuelpps <emanuelpages.ps@gmail.com>
2025-03-25 16:28:34 -0300

    feat: added randomInt function

---
+++ src/numbers/randomInt.ts
@@ -0,0 +1,5 @@
+export const randomInt = (min: number, max: number): number => {
+  const minNumber = Math.ceil(min);
+  const maxNumber = Math.floor(max);
+  return Math.floor(Math.random() * (maxNumber - minNumber + 1) + min);
+};
