e0e186d661404b5b2031367fa5972015b6be4188
emanuelpps <emanuelpages.ps@gmail.com>
2025-03-31 16:07:02 -0300

    feat: added provider to coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,25 +1,26 @@
-import path from 'path'
-import { defineConfig, mergeConfig } from 'vitest/config'
+import path from "path";
+import { defineConfig, mergeConfig } from "vitest/config";
 
-import viteConfig from './vite.config'
+import viteConfig from "./vite.config";
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
       globals: true,
-      setupFiles: './tests/setup',
+      setupFiles: "./tests/setup",
       alias: {
-        '@src': path.resolve(__dirname, '/src')
+        "@src": path.resolve(__dirname, "/src"),
       },
       coverage: {
+        provider: "v8",
         thresholds: {
           lines: 50,
           functions: 50,
           branches: 50,
-          statements: 50
-        }
-      }
-    }
+          statements: 50,
+        },
+      },
+    },
   })
-)
+);
