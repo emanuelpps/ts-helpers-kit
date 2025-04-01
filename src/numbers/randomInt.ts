47a9e2db3a15d54e09636bc895aec17b0158b0d1
emanuelpps <emanuelpages.ps@gmail.com>
2025-04-01 12:59:57 -0300

    test: added testing cases to numbers functions

--- src/numbers/randomInt.ts
+++ src/numbers/randomInt.ts
@@ -1,5 +1,15 @@
+import { isNumber } from '../checks';
+
 export const randomInt = (min: number, max: number): number => {
-  const minNumber = Math.ceil(min);
-  const maxNumber = Math.floor(max);
-  return Math.floor(Math.random() * (maxNumber - minNumber + 1) + min);
+  if (!isNumber(min) || !isNumber(max)) {
+    throw new Error('Parameters must be numbers');
+  }
+  
+  if (min > max) {
+    throw new Error('Min cannot be greater than max');
+  }
+
+  const minInt = Math.ceil(min);
+  const maxInt = Math.floor(max);
+  return Math.floor(Math.random() * (maxInt - minInt + 1) + minInt);
 };
