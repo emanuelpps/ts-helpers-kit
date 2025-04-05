906b45068425269b11cbf6d51e5befb9c829cb7e
emanuelpps <emanuelpages.ps@gmail.com>
2025-04-04 21:02:50 -0300

    feat: added getCookie function and test ok

--- src/storage/getCookie.ts
+++ src/storage/getCookie.ts
@@ -1,1 +1,14 @@
-//•	getCookie(name: string): string | null
+export const getCookie = (name: string): string | null => {
+    if (typeof document === "undefined") return null;
+  
+    const cookies = document.cookie ? document.cookie.split("; ") : [];
+  
+    for (const cookie of cookies) {
+      const [cookieName, ...rest] = cookie.split("=");
+      if (cookieName === name) {
+        return decodeURIComponent(rest.join("="));
+      }
+    }
+  
+    return null;
+  };  
