cebb981eebe6ae961137b06a9ae0f6e854657ab2
emanuelpps <emanuelpages.ps@gmail.com>
2025-04-07 20:15:50 -0300

    fix: remove unsolved features

--- src/validation/isEmail.ts
+++
@@ -1,4 +0,0 @@
-export function isEmail(email: string): boolean {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(email);
-}
--- src/validation/isStrongPassword.ts
+++
@@ -1,5 +0,0 @@
-export function isStrongPassword(password: string): boolean {
-  const strongPasswordRegex =
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,}$/;
-  return strongPasswordRegex.test(password);
-}
