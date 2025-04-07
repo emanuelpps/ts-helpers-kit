5736f40609e191bc08006e5b8aade42e7473ae7c
emanuelpps <emanuelpages.ps@gmail.com>
2025-04-07 20:16:52 -0300

    fix: thresholds fix in vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -14,10 +14,10 @@ export default mergeConfig(
       },
       coverage: {
         thresholds: {
-          lines: 50,
-          functions: 50,
-          branches: 50,
-          statements: 50,
+          lines: 90,
+          functions: 90,
+          branches: 90,
+          statements: 90,
         },
       },
     },
